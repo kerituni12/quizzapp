571eaf88700cc09cb79e090e3b1780fda1ae8a89
Hieunguyen <kerituni123@gmail.com>
2019-05-15 15:41:21 +0700

    del trash

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { View,Text, Button, } from 'react-native';
+import { View,Text, Button,TouchableOpacity,Image } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import {AccessToken, LoginManager } from 'react-native-fbsdk';
 import firebase from 'react-native-firebase';
+import Icon from 'react-native-vector-icons/FontAwesome';
 export default class WelcomeScreen extends Component {
   
   state = {    
     logout : true,
   }
-
+  
   _storeData = async (name,avatar) => {
     try {
       await AsyncStorage.setItem('@name:key', name);
@@ -47,10 +48,8 @@ export default class WelcomeScreen extends Component {
                 return Promise.reject(new Error('The user cancelled the request'));
             }
             console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`);
-            // get the access token
-            
+            // get the access token            
             return AccessToken.getCurrentAccessToken();
-
         })
         .then(data => {
             //console.log(data);
@@ -77,22 +76,29 @@ export default class WelcomeScreen extends Component {
             console.log(`Facebook login fail with error: ${error}`);
         });} 
 
-
+        
     render() {
      
-      return (
-      
-        <View  style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-         
+      return (      
+        <View  style={{ flex: 1, alignItems: 'center', justifyContent: 'center' ,backgroundColor:'#1adff7'}}>         
         {
          this.state.logout === true ?(<View>
-                <Button 
-                title="Login"
-                onPress={()=>this.onLoginFacebook()}/>
-                <Text style={{margin:20}}></Text>
-                <Button 
-                title="Guest Play"
-                onPress={()=>this.props.navigation.navigate('Dashboard')}/>
+        <Image
+              style={{height:100,width:100,alignSelf:'center'}}
+              source={{uri:'https://scontent.fsgn5-2.fna.fbcdn.net/v/t1.0-9/59685937_2362749297383070_3577093916547088384_n.jpg?_nc_cat=105&_nc_oc=AQlr-zAWksTlaZcUIQIng5vtA9Z_a02GmZsXWL9UqQj_IoIPCZr-DU9GV1Er6JSUXyBwZJfBe9dnrA3j7xjLSm4g&_nc_ht=scontent.fsgn5-2.fna&oh=c92875b7fd8a1597c8484cbe68bc8ee3&oe=5D5FE167'}}
+              />
+          <TouchableOpacity style={{padding:10, backgroundColor: '#fff',marginTop:40, borderRadius:5 }}
+          onPress={() => this.onLoginFacebook()}
+          >
+          <Text ><Icon style={{fontSize:20 }}  name='facebook'/>   Login With Facebook</Text>
+          </TouchableOpacity>
+          
+          <Text style={{margin:20, alignSelf:'center'}}> --------------- OR ---------------</Text>
+          <TouchableOpacity style={{padding:10, backgroundColor: '#fff',  borderRadius:5 }}
+          onPress={() => this.props.navigation.navigate('Dashboard')}
+          >
+          <Text ><Icon style={{fontSize:20 }}  name='user'/>        Play as Guest</Text>
+          </TouchableOpacity>          
               </View>
               )
          :  this.props.navigation.navigate('Dashboard')         
